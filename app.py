f052576fa18bd7cd71a222395452c70ee3395585
Radoslawss <radsip@icloud.com>
2024-07-05 20:41:39 +0200

    add main page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import csv
+import pandas as pd
 
 app = Flask(__name__)
 csv_file = 'data/temperature_data.csv'
@@ -18,6 +19,16 @@ def receive_json():
         writer.writerow([data['date'], data['time'], data['indoor_temperature'], data['outdoor_temperature']])
 
     return jsonify(response), 200
+
+def get_latest_temperatures():
+    df = pd.read_csv('data/temperature_data.csv')
+    latest_record = df.iloc[-1]
+    return round(latest_record['indoor_temperature'], 1), round(latest_record['outdoor_temperature'], 1)
+
+@app.route('/')
+def index():
+    indoor_temp, outdoor_temp = get_latest_temperatures()
+    return render_template('index.html', indoor_temp=indoor_temp, outdoor_temp=outdoor_temp)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
